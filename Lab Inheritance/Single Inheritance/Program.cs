2fb25e025296faf4e8243b1108bd1d6abb4e8ca0
YovkovIT <yovkovyoan88@gmail.com>
2024-04-21 12:58:03 +0300

    basics inheritance

---
+++ Lab Inheritance/Single Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy doggie = new Puppy();
+            doggie.Eat();
+            doggie.Bark();
+            doggie.Weep();
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
