520bc0ac49b0de48336b6c100e88ce059eb4a3b1
YovkovIT <yovkovyoan88@gmail.com>
2024-04-22 12:38:19 +0300

    Custom list and some stack

---
+++ CustomRandomList/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList mylist = new RandomList();
+
+            mylist.Add("Ivo");
+            mylist.Add("Damqn");
+            mylist.Add("Yavor");
+
+            mylist.Display();
+
+            try
+            {
+                string removedItem = mylist.RemoveRandom();
+                Console.WriteLine(removedItem);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+            mylist.Display();
+        }
+    }
+    public class RandomList
+    {
+       public List<string> list;
+       private Random random;
+
+        public RandomList()
+        {
+            list = new List<string>();
+            random = new Random();
+        }
+        public int Count()
+        {
+            return list.Count;
+        }
+        public void Add(string value)
+        {
+            list.Add(value);
+        }
+        public void Remove(string value)
+        {
+            list.Remove(value);
+        }
+        public void Clear()
+        {
+            list.Clear();
+        }
+        public void Insert(string value)
+        {
+            list.Contains(value);
+        }
+
+        public string RemoveRandom()
+        {
+            if(list.Count == 0)
+            {
+                throw new InvalidOperationException("String is empty");
+            }
+
+            int index = random.Next(list.Count);
+            string randomElement = list[index];
+            list.RemoveAt(index);
+            return randomElement;
+        }
+        public void Display()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ CustomStack/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            stack.Push("Ivo");
+            stack.Push("Magdalena");
+            stack.Push("Ivana");
+
+            try
+            {
+                Console.WriteLine($"This is Empty or no? {stack.IsEmpty()}");
+
+                Stack<string> additionalStack = new Stack<string>();
+                additionalStack.Push("Viktoriq");
+                additionalStack.Push("Adelina");
+                additionalStack.Push("Stefan");
+
+                stack.AddRange(additionalStack);
+                Console.WriteLine($"This is Empty or no? {stack.IsEmpty()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine (ex.ToString());
+            }
+        }
+    }
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+        public Stack<string> AddRange(Stack<string> additionalStack)
+        {
+            foreach (string item in additionalStack)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+    }
+}
